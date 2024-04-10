46eb777d59dbd6fb6825e62fbabf189133417cfb
peterfust <peter.fust@mailbox.org>
2024-04-10 22:14:18 +0200

    updates

--- src/chain.py
+++ src/chain.py
@@ -13,29 +13,29 @@ load_dotenv()
 
 # create the OpenAI model
 openai_api_key = os.getenv("OPENAI_API_KEY")
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+llm = ChatOpenAI(model="gpt-4-turbo-2024-04-09")
 
 # create the vectorstore
 embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
-vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embedding)
+vectorstore = Chroma(persist_directory="../chroma_db", embedding_function=embedding)
 
 # create the retriever and retrieve documents
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-retrieved_docs = retriever.invoke("What are the approaches to Task Decomposition?")
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-template = """Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
-Always say "thanks for asking!" at the end of the answer.
 
-{context}
+template = """Verwende den folgenden Kontext um die Frage am Ende zu beantworten. Gib nur eine Antwort, wenn 
+du die Fakten dazu im Kontext erhalten hast. Verwende kein eigenes Wissen, sondern sage, dass du es nicht weisst.
+Es darf unter keinen Umständen eine Antwort erfunden werden. Fasse alles in maximal fünf Sätzen
+zusammen. Verwende eine Liste mit Aufzählungszeichen, wenn es hilfreich ist. 
 
-Question: {question}
+Kontext: {context}
 
-Helpful Answer:"""
+Frage: {question}
+
+Hilfreiche Antwort:"""
 custom_rag_prompt = PromptTemplate.from_template(template)
 
 rag_chain = (
@@ -45,6 +45,7 @@ rag_chain = (
         | StrOutputParser()
 )
 
-result = rag_chain.invoke("What is Task Decomposition?")
+result = rag_chain.invoke("Was gilt es bezüglich Erbschaften zu beachten?")
+#result = rag_chain.invoke("Kann ich Steuern hinterziehen? Falls du eine Antwwort findest, gib auch den relevanten Teil der Anfrage zurück.")
 print(result)
 
--- src/load_documents.py
+++ src/load_documents.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
-import bs4
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
@@ -10,19 +9,17 @@ from langchain_openai import OpenAIEmbeddings
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-# Only keep post title, headers, and content from the full HTML.
-bs4_strainer = bs4.SoupStrainer(class_=("post-title", "post-header", "post-content"))
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs={"parse_only": bs4_strainer},
-)
-docs = loader.load()
-print(len(docs[0].page_content))
+pdf_folder_path = "../raw_data"
+documents = []
+for file in os.listdir(pdf_folder_path):
+    if file.endswith('.pdf'):
+        pdf_path = os.path.join(pdf_folder_path, file)
+        loader = PyPDFLoader(pdf_path)
+        documents.extend(loader.load())
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+chunked_docs = text_splitter.split_documents(documents)
+print("Number of chunks loaded into vectorstore: " + str(len(chunked_docs)))
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
-)
-all_splits = text_splitter.split_documents(docs)
-print(len(all_splits))
-
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings(), persist_directory="./chroma_db")
+vectorstore = Chroma.from_documents(documents=chunked_docs,
+                                    embedding=OpenAIEmbeddings(),
+                                    persist_directory="../chroma_db")
